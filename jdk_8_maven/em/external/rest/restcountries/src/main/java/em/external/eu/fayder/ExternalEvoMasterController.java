ee7659c52387bc8da0233590b2fb7b21fa956a10
Ruikai Huang <rkh@gatech.edu>
2025-03-03 19:06:27 -0500

    hardcode jacoco agent path

--- jdk_8_maven/em/external/rest/restcountries/src/main/java/em/external/eu/fayder/ExternalEvoMasterController.java
+++ jdk_8_maven/em/external/rest/restcountries/src/main/java/em/external/eu/fayder/ExternalEvoMasterController.java
@@ -44,7 +44,7 @@ public class ExternalEvoMasterController extends ExternalSutController {
                 new ExternalEvoMasterController(controllerPort, jarLocation, sutPort, timeoutSeconds, command);
 
 
-        controller.setJaCoCo("jaCoCoAgentLocation=/home/rhuang329/aster-it/target/realtime-jacoco-agent-1.0-SNAPSHOT.jar", "", "", 8000);
+        controller.setJaCoCo("jaCoCoAgentLocation=/home/rhuang329/aster-it/target/realtime-jacoco-agent-1.0-SNAPSHOT.jar", "/home/rhuang329/jacoco-0.8.12/lib/jacococli.jar", "/home/rhuang329/EvoMasterv3.4.0/exec/restcountries__wbEXP__12410__jacoco.exec", 8000);
         InstrumentedSutStarter starter = new InstrumentedSutStarter(controller);
 
         starter.start();
